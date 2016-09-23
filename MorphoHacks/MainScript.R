36caeaaa417be160110fa718580bedd3a0de29d0
Justin <justin@ratchet.nceas.ucsb.edu>
2016-09-23 12:29:36 -0700

    Rename variable in MainScript.R
    Also rewrite README and fix typos.

--- MorphoHacks/MainScript.R
+++ MorphoHacks/MainScript.R
@@ -1,14 +1,14 @@
 # User-dependent variables ------------------------------------------------
-# Absolute path to folder containing the scripts
+# Absolute path to folder containing MainScript.R and WriteUniqueFieldValues.R
 scripts_path <- ""
-# Absolute path to your file to be processed
+# Absolute path to your data-file input
 read_dir <- ""
-# Absolute path to directory in which to write script outputs
+# Absolute path to the directory in which to write script outputs
 write_dir <- ""
-# A custom code used in your data to indicate any missing data. This excludes
-# blank cells (represented in R as an empty character vector or string) as well
-# as R's own code, "NA".
-custom_code <- ""
+# A custom string used in your data to indicate any missing data. If your
+# data represents missing values by empty cells, or even R's "NA" (without the
+# quotes), then do not modify this variable's value.
+custom_string_for_missing_values <- ""
 
 # Non-user operations -----------------------------------------------------
 # Clear environment
@@ -21,4 +21,4 @@ source(file.path(scripts_path, "WriteUniqueFieldValues.R"))
 # Call function with user inputs
 WriteUniqueFieldValues(.read_dir = read_dir,
                        .write_dir = write_dir,
-                       .missing_code = custom_code)
+                       .missing_code = custom_string_for_missing_values)
